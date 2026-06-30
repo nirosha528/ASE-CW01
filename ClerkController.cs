3d612ecd8fa71c1d7897f7a8f0465b684af2047c
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-30 11:18:31 +0530

    Add files via upload

---
+++ ClerkController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotelreservation.Controllers
+{
+    public class ClerkController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            var role = HttpContext.Session.GetString("Role");
+
+            if (role != "Clerk")
+                return RedirectToAction("Login", "Account");
+
+            return View();
+        }
+    }
+}
