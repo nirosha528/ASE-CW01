e6169ef0ce9a886ceac16dcbfab74ffe60ebed02
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-30 11:25:57 +0530

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotelreservation.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty; // manager/clerk/customer/travelagency
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+    }
+}
