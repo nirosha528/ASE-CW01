9e71ce7940372bb4af180161fe842cb5a8da3549
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-30 11:25:06 +0530

    Add files via upload

---
+++ ReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Hotelreservation.Models
+{
+    public class ReportViewModel
+    {
+        public int TotalReservations { get; set; }
+        public int TotalUsers { get; set; }
+        public int TotalRooms { get; set; }
+
+        public List<string> Months { get; set; } = new();
+        public List<int> ReservationCounts { get; set; } = new();
+    }
+}
