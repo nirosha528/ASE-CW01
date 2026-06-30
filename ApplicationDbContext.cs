7de52f6022055bd97b695b56127dcf20cccccb5d
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-30 11:21:41 +0530

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Hotelreservation.Models;
+
+namespace Hotelreservation.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<Payment> Payments { get; set; }
+
+        public DbSet<ReservationRequest> ReservationRequests { get; set; }
+    }
+}
