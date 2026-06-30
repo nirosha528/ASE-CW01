388bb2dfcf0625470565a21ba3bb9d2b78ea13b1
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-30 11:42:22 +0530

    Add files via upload

---
+++ EmailServices.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Hotelreservation.Services
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void SendEmail(string subject, string body)
+        {
+            var email = _config["EmailSettings:Email"];
+            var password = _config["EmailSettings:Password"];
+
+            var client = new SmtpClient(_config["EmailSettings:Host"])
+            {
+               // Port = int.Parse(_config["EmailSettings:Port"]),
+                Credentials = new NetworkCredential(email, password),
+                EnableSsl = true
+            };
+
+            var mail = new MailMessage
+            {
+               // From = new MailAddress(email),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            //Manager email
+            mail.To.Add("hotel@gmail.com");
+
+            client.Send(mail);
+        }
+    }
+}
