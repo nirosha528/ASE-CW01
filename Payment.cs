f5ed818b612b8a5f4e3ec0d58404aa0d8ce86d6d
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-30 11:24:52 +0530

    Add files via upload

---
+++ Payment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotelreservation.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int PaymentId { get; set; }
+
+        public int ReservationId { get; set; }
+
+        public string CustomerName { get; set; } = "";
+
+        public decimal RoomCharge { get; set; }
+
+        public decimal RestaurantCharge { get; set; }
+
+        public decimal RoomServiceCharge { get; set; }
+
+        public decimal LaundryCharge { get; set; }
+
+        public decimal TelephoneCharge { get; set; }
+
+        public decimal ClubFacilityCharge { get; set; }
+
+        public decimal TravelAgencyDiscount { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public string PaymentMethod { get; set; } = "";
+
+        public string Status { get; set; } = "Paid";
+
+        public DateTime PaymentDate { get; set; } = DateTime.Now;
+    }
+}
