a39bf47278bffadf06bd61de8d197045509f0528
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-30 11:21:11 +0530

    Add files via upload

---
+++ TravelAgencyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotelreservation.Controllers
+{
+    public class TravelAgencyController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            var role = HttpContext.Session.GetString("Role");
+
+            if (role != "travelagency")
+                return RedirectToAction("Login", "Account");
+
+            return View();
+        }
+    }
+}
