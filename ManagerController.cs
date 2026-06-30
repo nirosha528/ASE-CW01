cc20b3deab05dbb5370c155347ff7a2fecff0ea9
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-30 11:19:29 +0530

    Add files via upload

---
+++ ManagerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotelreservation.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            var role = HttpContext.Session.GetString("Role");
+
+            if (role != "Manager")
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View();
+        }
+    }
+}
