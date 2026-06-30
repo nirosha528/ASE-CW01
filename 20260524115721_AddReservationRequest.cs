a1b4c59e9c18a69089ebb3c845314e9d75618ad4
nirosha528 <wanigasekaranirosha777@gmail.com>
2026-06-30 11:23:04 +0530

    Add files via upload

---
+++ 20260524115721_AddReservationRequest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotelreservation.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReservationRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ReservationRequests");
+
+            migrationBuilder.RenameColumn(
+                name: "RoomId",
+                table: "ReservationRequests",
+                newName: "RoomNo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RoomNo",
+                table: "ReservationRequests",
+                newName: "RoomId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "ReservationRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
